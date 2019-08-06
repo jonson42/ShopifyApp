6dcead6a5c03546563766deee9cf247275db3997
Nghiem <quocquoc42@gmail.com>
2019-08-07 01:08:28 +0700

    change screen Settings

---
+++ ShopifyBackupWeb/Models/EmailModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopifyBackupWeb.Models
+{
+    public class EmailModel
+    {
+        public string Host { get; set; }
+        public string EmailSend { get; set; }
+        public string PassSend { get; set; }
+    }
+}
